660f656c94a819843d558c12f0b7dce605645880
Евгения <zhoonya1@rambler.ru>
2020-04-17 22:43:18 +0300

    Добавлены обработчик для Esk и отрисовка пустой доски.

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -37,7 +37,7 @@ export default class SiteMenu {
 
   getElement() {
     if (!this._element) {
-      return createElement(this.getTemplate());
+      this._element = createElement(this.getTemplate());
     }
     return this._element;
   }
